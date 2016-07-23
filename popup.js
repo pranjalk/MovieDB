4ea255b42e344f6f128d3a15bbc5b2c4e723a7cf
Pranjal Kumar <pranjal.kr94@gmail.com>
2016-07-23 16:07:18 +0530

    Initial commit

---
+++ popup.js
@@ -0,0 +1,19 @@
+chrome.tabs.executeScript( {
+    code: "window.getSelection().toString();"
+}, function(selection) {
+  var movieName = selection[0];
+  var res = movieName.split(" ");
+  var conCat = res.join('+');
+  //console.log(movieName);
+  //document.getElementById("output").innerHTML = conCat; //check movie name;
+  var xhttp = new XMLHttpRequest();
+  apiQuery = "http://www.omdbapi.com/?t=" + conCat + "&y=&plot=short&r=json";
+  //document.getElementById("output").innerHTML = apiQuery; //create api query
+  xhttp.open("GET", apiQuery, false);
+  xhttp.send();
+  var result = JSON.parse(xhttp.responseText);
+  console.log(result);
+  document.getElementById("titleRating").innerHTML = result.imdbRating; // imdbRating
+  document.getElementById("movieTitle").innerHTML = result.Title; // imdbTitle
+  document.getElementById("moviePoster").src = result.Poster;
+});
